b5d814a09e10b49d49b525efc31dbd8280bcd466
Johan Hagstrom <johan.hagstrom@mottmac.com>
2020-11-10 21:10:06 +0000

    tests added

--- tests/routely_test.py
+++ tests/routely_test.py
@@ -3,24 +3,7 @@
 import numpy as np
 import unittest
 from routely.routely import Route
-%load_ext autoreload
 
-# Tests
-#%%
-%autoreload
-
-x = [0, 10, 20, 30]
-y = [0, 20, 40, 60]
-
-r = Route(x=x, y=y)
-r.plotroute()
-r.d
-r.interoplate(inplace=True)
-r.plotroute()
-#%%
-np.array(r.route)
-list(np.arange(0, np.array(r.d).max(), 1))
-# Class tests
 
 class routely_test(unittest.TestCase):
 
@@ -30,3 +13,18 @@ class routely_test(unittest.TestCase):
 
         output = Route.distance_between_two_points(p1, p2)
         self.assertEqual(output, 5)
+
+    def test_calculate_distance(self):
+        x = [0, 7.5, 15, 22.5, 30]
+        y = [0, 0, 0, 0, 0]
+        r = Route(x, y)
+
+        output = np.diff(np.array(r.d))
+        self.assertEqual(7.5, output[0])
+
+        x = [0, 3, 6, 9, 12]
+        y = [0, 4, 8, 12, 16]
+        r = Route(x, y)
+
+        output = np.diff(np.array(r.d))
+        self.assertEqual(5, output[0])
