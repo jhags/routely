50238e9ec74757600c07a37444660dd7d098b4c0
Johan Hagstrom <johan.hagstrom@mottmac.com>
2020-11-10 21:10:14 +0000

    dev temp script

---
+++ tests/temp.py
@@ -0,0 +1,25 @@
+''' Routely tests '''
+# Packages
+import numpy as np
+
+from routely import Route
+
+%load_ext autoreload
+#%%
+%autoreload 2
+x = [0, 5, 15, 20, 10]
+y = [0, 10, 20, 10, 5]
+
+r = Route(x, y)
+
+r.interoplate(kind='cubic', num=100, inplace=True)
+r.plotroute()
+r2 = r.rotate(100, inplace=False)
+r2.plotroute()
+r2.centre()
+
+# r.reset_origin(new_origin=(100, 100), inplace=True)
+# r.plotroute(markers=False)
+
+# r.interoplate(kind='cubic', num=100, inplace=True)
+# r.plotroute(markers=False)
