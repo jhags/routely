8a1329e5639498f2d8ae3a91725a056ba61563f1
Johan Hagstrom <johan.hagstrom@mottmac.com>
2020-11-12 21:52:47 +0000

    setup requirements packages removed

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     version='0.1.0',
     packages=find_packages(include=['routely', 'routely.*']),
     install_requires=[
-        'pandas',
         'numpy',
         'scipy',
         'matplotlib'
