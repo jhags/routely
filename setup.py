bc094dda94cbf0a39672cb904633f3ad39b24ab6
Johan Hagstrom <johan.hagstrom@mottmac.com>
2020-12-11 09:45:56 +0000

    setup updated for pypi publishing

--- setup.py
+++ setup.py
@@ -1,11 +1,27 @@
 from setuptools import setup, find_packages
 
+# requirement file
 with open('requirements.txt') as f:
     required = f.read().splitlines()
+
+# readme file
+with open('README.rst') as f:
+    readme = f.read()
+
 
 setup(
     name='routely',
     version='0.1.0',
+    description='Common operations and transformations on routes represented by a 2D line in xy space.',
+    long_description=readme,
+    url='https://github.com/jhags/routely',
+    author='J Hagstrom',
+    license='MIT',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
     packages=find_packages(include=['routely', 'routely.*']),
     install_requires=required,
     tests_require=['pytest', 'pytest-cov', 'coveralls']
